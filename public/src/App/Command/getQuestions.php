467ac3e86b391edc7beec1be6eeafeacb3b46171
robhsc <rob.humar@socialchain.com>
2019-03-20 15:09:40 +0000

    FIX: Added check for question existence

--- public/src/App/Command/getQuestions.php
+++ public/src/App/Command/getQuestions.php
@@ -49,21 +49,36 @@ class getQuestions extends SymfonyCommand
             $io->writeln(array('<info>Logged in.</info>','Fetching story info...'));
             $story = $this->ig->story->getUserReelMediaFeed($this->ig->account_id);
 
+            // Does the story have any items?
             if($story->hasItems()){
+
+              // Get the items
               $items = $story->getItems();
+
+              // Loop through the items
               foreach ($items as $item) {
+
                 $question = $item->getStoryQuestions();
-                $io->writeln(array('<info>' . $question[0]->getQuestionSticker()->getQuestion() . '</info>'));
-                if($item->hasStoryQuestionResponderInfos()) {
-                  $responseCount = $item->getStoryQuestionResponderInfos();
-                  $io->writeln(array('Responses: ' . $responseCount[0]->getQuestionResponseCount()));
+
+                // Is there a question?
+                if($question) {
+                  $io->writeln(array('<info>' . $question[0]->getQuestionSticker()->getQuestion() . '</info>'));
+
+                  // Does it have any responses?
+                  if($item->hasStoryQuestionResponderInfos()) {
+                    $responseCount = $item->getStoryQuestionResponderInfos();
+                    $io->writeln(array('Responses: ' . $responseCount[0]->getQuestionResponseCount()));
+
+                    // Has the list been requested?
                     if($input->getOption('list')) {
                       foreach ($responseCount[0]->getResponders() as $response) {
                         $io->writeln(array($response->getUser()->getUsername() . ': ' . $response->getResponse()));
                       }
                     }
-                } else {
-                  $io->writeln(array('No responses'));
+                  } else {
+                    // No responses
+                    $io->writeln(array('No responses'));
+                  }
                 }
               }
             } else {
